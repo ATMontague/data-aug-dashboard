3c143eed1e23fcbfdec91c3b9c345006f80f28b7
adam <atmontague10@gmail.com>
2023-04-30 20:36:19 -0400

    gist update

--- pages/6_Beating_the_Benchmark.py
+++ pages/6_Beating_the_Benchmark.py
@@ -6,7 +6,7 @@ from streamlit_embedcode import github_gist
 
 st.title('Beating the Benchmark')
 
-github_gist("https://gist.github.com/ATMontague/7177c0247cccbc48eed4a7729a45da81")
+github_gist("https://gist.github.com/ATMontague/4aad585d943ea074e4d575946901e87c")
 
 url = "https://drive.google.com/file/d/1NNNn0qGm0AhG1qzW8pkyb-a_7tVXYXA3/view?usp=sharing"
 st.write("Click here to open and edit the notebook in [Google Colab](%s)" % url)
